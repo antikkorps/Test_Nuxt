de8d0068e5f6b8c524f7bc85248dc9040adb44c4
Franck <franckvienot7@gmail.com>
2023-11-13 17:44:54 +0100

    added title

--- components/UpdatedStories.vue
+++ components/UpdatedStories.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="flex justify-center">
+    <h2>Derniers Tats</h2>
     <div class="my-10">
-      <img class="h-16 w-16 mx-5 rounded-full" src="https://picsum.photos/200" alt="" />
+      <img class="h-16 w-16 mx-4 rounded-full" src="https://picsum.photos/200" alt="" />
     </div>
     <div class="my-10">
-      <img class="h-16 w-16 mx-5 rounded-full" src="https://picsum.photos/200" alt="" />
+      <img class="h-16 w-16 mx-4 rounded-full" src="https://picsum.photos/200" alt="" />
     </div>
     <div class="my-10">
-      <img class="h-16 w-16 mx-5 rounded-full" src="https://picsum.photos/200" alt="" />
+      <img class="h-16 w-16 mx-4 rounded-full" src="https://picsum.photos/200" alt="" />
     </div>
     <div class="my-10">
-      <img class="h-16 w-16 mx-5 rounded-full" src="https://picsum.photos/200" alt="" />
+      <img class="h-16 w-16 mx-4 rounded-full" src="https://picsum.photos/200" alt="" />
     </div>
     <div class="my-10">
-      <img class="h-16 w-16 mx-5 rounded-full" src="https://picsum.photos/200" alt="" />
+      <img class="h-16 w-16 mx-4 rounded-full" src="https://picsum.photos/200" alt="" />
     </div>
   </div>
 </template>
