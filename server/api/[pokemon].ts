32611825a12dc809e9e4c542f7517df5c5040c4b
Franck <franckvienot7@gmail.com>
2023-11-13 10:47:45 +0100

    testing middleware and api

--- server/api/[pokemon].ts
+++ server/api/[pokemon].ts
@@ -1,18 +1,18 @@
 export default defineEventHandler(async (event) => {
   try {
-    const { pokemon } = event.context.params;
-    const response = await $fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const { pokemon } = event.context.params
+    const response = await $fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
 
-    console.log(event.context);
+    console.log(event.context)
 
     const idealResponse = {
       id: response.id,
       name: response.name,
       sprite: response.sprites.front_default,
-    };
+    }
 
-    return idealResponse;
+    return idealResponse
   } catch (err) {
-    throw createError({ statusCode: 404, message: "can't find the response" });
+    throw createError({ statusCode: 404, message: "can't find the response" })
   }
-});
+})
