ceb7e2ee31e4a8a8c69984680b28abaf1da46ce8
Franck <franckvienot7@gmail.com>
2023-11-15 10:48:36 +0100

    made components of the cardActionsButton to make it more readable and auto-document the code

---
+++ components/CardActionsGroup.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-wrap justify-between my-5 mx-2">
+    <div class="flex">
+      <CardActionsLikeButton />
+      <CardActionsMessageButton />
+      <CardActionsShareButton />
+    </div>
+    <div>
+      <CardActionsBookmarkButton />
+    </div>
+  </div>
+</template>
--- components/Tats.vue
+++ components/Tats.vue
@@ -8,6 +8,7 @@
         src="https://picsum.photos/200/300"
         alt="Tats Illustration"
       />
+      <CardActionsGroup />
       <div class="px-6 py-4">
         <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
         <p class="text-gray-700 text-base">
